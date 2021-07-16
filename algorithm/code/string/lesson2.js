78ad69e3af171e4c5b81f4d3efd1755e83e0e078
emilwang <emil@naryo.com>
2021-07-16 11:17:56 +0800

    Javascript algorithm

---
+++ algorithm/code/string/lesson2.js
@@ -0,0 +1,22 @@
+function subStr(str) {
+    let count = 0, // 连续子串的数量
+        curLen = 1, // 当前遍历的子串长度
+        preLen = 0, // 上次遍历的子串长度
+        strArr = str.split(''),
+        res = []
+    // 从第二项开始遍历
+    for (let i = 1, len = strArr.length; i < len; i++) {
+        if (strArr[i] === strArr[i - 1]) { // 如果当前子串等于上个子串，curLen++
+            curLen++
+        } else { // 不等于子串，重置
+            preLen = curLen 
+            curLen = 1
+        }
+        if (preLen >= curLen) {
+            
+            count++
+        }
+    }
+    return count;
+}
+export default subStr
